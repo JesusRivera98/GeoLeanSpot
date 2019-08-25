dc0f62ec9d00148edd75937a75dd5abe3f8e260c
Jesus Rivera Flores <A00820643@itesm.mx>
2019-08-25 11:15:34 -0500

    actualizacion

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -13,40 +13,59 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
-var labels = [];
-var data = [];
-var database = firebase.database();
-window.onload = updateChart();
+var labels_west = [];
+var data_west = [];
+var labels_east = [];
+var data_east = [];
+window.onload = updateChartWest();
+window.onload = updateChartEast();
 
-function getData(){
+function getDataWest(){
   places = database.ref('Places/Centro_Estudiantil_West').once('value').then(function(snapshot) {
       var spots = snapshot.val();
       for (key in spots){
           for(key2 in spots[key]){
               var average = spots[key][key2].reduce((a,b) => a+b, 0) / 3;
               var shortKey = new Date(Number(key2.slice(0,10) * 1000));
-              labels.push((shortKey.getHours().toString()) + ":" + (shortKey.getMinutes()));
-              data.push(average);
+              labels_west.push((shortKey.getHours().toString()) + ":" + (shortKey.getMinutes()));
+              data_west.push(average);
           }
       }
   });
 };
 
+function getDataEast(){
+  places = database.ref('Places/Centro_Estudiantil_East').once('value').then(function(snapshot) {
+    var spots = snapshot.val();
+      for (key in spots){
+          for(key2 in spots[key]){
+              var average = spots[key][key2].reduce((a,b) => a+b, 0) / 3;
+              var shortKey = new Date(Number(key2.slice(0,10) * 1000));
+              labels_east.push((shortKey.getHours().toString()) + ":" + (shortKey.getMinutes()));
+              data_east.push(average);
+          }
+      }
+  });
+};
+
+
+
 setInterval(
   function () {
-    updateChart();
+    updateChartWest();
+    updateChartEast();
   }, 
   10000 // every 15 seconds
 );
 
-function updateChart(){
-  getData();
+function updateChartWest(){
+  getDataWest();
   // Area Chart Example
-  var ctx = document.getElementById("myAreaChart");
+  var ctx = document.getElementById("centroWest");
   var myLineChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: labels,
+      labels: labels_west,
       datasets: [{
         label: "People in the area",
         lineTension: 0.3,
@@ -60,7 +79,104 @@ function updateChart(){
         pointHoverBorderColor: "rgba(78, 115, 223, 1)",
         pointHitRadius: 10,
         pointBorderWidth: 2,
-        data: data,
+        data: data_west,
+      }],
+    },
+    options: {
+      maintainAspectRatio: false,
+      layout: {
+        padding: {
+          left: 10,
+          right: 25,
+          top: 25,
+          bottom: 0
+        }
+      },
+      scales: {
+        xAxes: [{
+          time: {
+            unit: 'date'
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          ticks: {
+            maxTicksLimit: 7
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            maxTicksLimit: 5,
+            padding: 10,
+            // Include a dollar sign in the ticks
+            callback: function(value, index, values) {
+              return number_format(value);
+            }
+          },
+          gridLines: {
+            color: "rgb(234, 236, 244)",
+            zeroLineColor: "rgb(234, 236, 244)",
+            drawBorder: false,
+            borderDash: [2],
+            zeroLineBorderDash: [2]
+          }
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        titleMarginBottom: 10,
+        titleFontColor: '#6e707e',
+        titleFontSize: 14,
+        borderColor: '#dddfeb',
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: false,
+        intersect: false,
+        mode: 'index',
+        caretPadding: 10,
+        callbacks: {
+          label: function(tooltipItem, chart) {
+            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+            return datasetLabel + ':' + number_format(tooltipItem.yLabel);
+          }
+        }
+      }
+    }
+  });
+  var westNum = document.getElementById("west");
+  westNum.innerHTML = Math.floor(data_west[data_west.length - 1]);
+  labels_west = [];
+  data_west = [];
+}
+
+function updateChartEast(){
+  getDataEast();
+  // Area Chart Example
+  var ctx = document.getElementById("centroEast");
+  var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: labels_east,
+      datasets: [{
+        label: "People in the area",
+        lineTension: 0.3,
+        backgroundColor: "rgba(78, 115, 223, 0.05)",
+        borderColor: "rgba(78, 115, 223, 1)",
+        pointRadius: 3,
+        pointBackgroundColor: "rgba(78, 115, 223, 1)",
+        pointBorderColor: "rgba(78, 115, 223, 1)",
+        pointHoverRadius: 3,
+        pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+        pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+        pointHitRadius: 10,
+        pointBorderWidth: 2,
+        data: data_east,
       }],
     },
     options: {
@@ -130,8 +246,10 @@ function updateChart(){
       }
     }
   });
-  labels = []
-  data = []
+  var eastNum = document.getElementById("east");
+  eastNum.innerHTML = Math.floor(data_east[data_east.length - 1]);
+  labels_east = [];
+  data_east = [];
 }
 
 
